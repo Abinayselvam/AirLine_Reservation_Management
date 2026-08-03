46dbc7527737b447fbdc5828218f028e2f975458
AbinayaSelvam <sabinaya352@gmail.com>
2026-08-03 11:06:45 +0530

    Added the models and enum for airline_Management

---
+++ app/src/main/java/Examples/model/AirlineStaff.java
@@ -0,0 +1,9 @@
+package Examples.model;
+
+public class AirlineStaff extends User {
+
+    @Override
+    public void showDashboard() {
+        System.out.println("Airline Staff Dashboard");
+    }
+}
